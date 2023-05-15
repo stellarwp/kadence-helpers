b76ab3b295153216f52086e6c4c1da3a27571a27
Mark Woodard <mark.c.woodard@gmail.com>
2023-05-15 01:25:02 -0500

    updated getting repeaters to store value in local storage for acces from other components

--- src/get-acf-repeaters-for-source/index.js
+++ src/get-acf-repeaters-for-source/index.js
@@ -2,6 +2,9 @@ const { repeatersEndpoint } = window.kadenceDynamicParams;
 const { apiFetch } = wp;
 const { addQueryArgs } = wp.url;
 const { addFilter } = wp.hooks;
+const {
+	localStorage,
+} = window;
 
 /**
  * Returns an array of repeater fields for a source
@@ -10,37 +13,36 @@ const { addFilter } = wp.hooks;
  * @returns {array} All repeater fields associated with the source
  */
 export default function getACFRepeatorsForSource( source, onFinish ) {
+
     if ( 'function' == typeof(onFinish) ) {
-		apiFetch( {
-			path: addQueryArgs(
-				repeatersEndpoint,
-				{
-                    source: source,
+        const repeatersKey = 'kadenceRepeaters-' + source;
+        const localRepeatersForSource = JSON.parse( localStorage.getItem( repeatersKey ) );
+
+        if( localRepeatersForSource ) {
+            onFinish( localRepeatersForSource );
+            return localRepeatersForSource;
+        } else {
+            apiFetch( {
+                path: addQueryArgs(
+                    repeatersEndpoint,
+                    {
+                        source: source,
+                    }
+                ),
+            } )
+            .then( ( response ) => {
+                if( 'object' === typeof( response ) && 1 >= Object.keys(response).length ) {
+                    localStorage.setItem( repeatersKey, JSON.stringify( response ) );
+
+                    onFinish( response );
+                    return response
                 }
-			),
-		} )
-        .then( ( response ) => {
-            if( 'object' === typeof( response ) && 1 >= Object.keys(response).length ) {
-                onFinish( response );
-                return response
-            }
-        } )
-        .catch( ( e ) => {
-            // Nothing needed here.
-        } );
-        onFinish( null );
-        return null
+            } )
+            .catch( ( e ) => {
+                // Nothing needed here.
+            } );
+            onFinish( null );
+            return null
+        }
     }
-    // return content;
-    // var repeaterKeys = []
-    // if( record && 'object' == typeof( record['acf'] ) && 1 <= Object.keys(record['acf']).length ) {
-    //     Object.keys(record['acf']).forEach( key => {
-    //         const field = record['acf'][key];
-    //         if ( 'object' == typeof( field ) && field && 1 <= field.length && 'undefined' != typeof( field[0] ) ) {
-    //             repeaterKeys.push( { value: key, label: key } );
-    //         }
-    //     });
-    // }
-    // onFinish( repeaterKeys );
-    // return true;
 }
