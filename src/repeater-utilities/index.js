ad4b28380d071790f356ef05ad928f586d59dc26
Mark Woodard <mark.c.woodard@gmail.com>
2023-06-29 17:44:31 -0500

    getting repeaters from the function that already does it better like the custom field select somponent

--- src/repeater-utilities/index.js
+++ src/repeater-utilities/index.js
@@ -1,4 +1,4 @@
-const { repeatersEndpoint, repeaterDataEndpoint } = window.kadenceDynamicParams;
+const { repeatersEndpoint, repeaterDataEndpoint, dynamicFieldsEndpoint } = window.kadenceDynamicParams;
 const { apiFetch } = wp;
 const { addQueryArgs } = wp.url;
 const { addFilter } = wp.hooks;
@@ -12,7 +12,7 @@ const {
  * @param {*} source The post id (or option) source
  * @returns {array} All repeater fields associated with the source
  */
-export function getRepeatersForSource( source, onFinish ) {
+export function getRepeatersForSource( source, contextPost, onFinish ) {
     if ( 'function' == typeof(onFinish) ) {
         const repeatersKey = 'kadenceRepeaters-' + source;
         
@@ -22,27 +22,32 @@ export function getRepeatersForSource( source, onFinish ) {
             onFinish( localRepeatersForSource );
             return localRepeatersForSource;
         } else {
+            const props = {
+                source:source,
+                type:"repeaters",
+                contextPost:contextPost,
+                useRepeaterContext:true
+            };
+
             apiFetch( {
                 path: addQueryArgs(
-                    repeatersEndpoint,
-                    {
-                        source: source,
-                    }
+                    dynamicFieldsEndpoint,
+                    getQuery( props, contextPost )
                 ),
             } )
-            .then( ( response ) => {
-                if( 'object' === typeof( response ) && 1 <= Object.keys(response).length ) {
-                    localStorage.setItem( repeatersKey, JSON.stringify( response ) );
-
-                    onFinish( response );
-                    return response
-                }
-            } )
-            .catch( ( e ) => {
-                // Nothing needed here.
-            } );
-            onFinish( null );
-            return null
+                .then( ( response ) => {
+                    //this.setState( { fields, loaded: true } );
+                    if( 'object' === typeof( response ) && 1 <= Object.keys(response).length ) {
+                        localStorage.setItem( repeatersKey, JSON.stringify( response ) );
+    
+                        onFinish( response );
+                        return response
+                    }
+                } )
+                .catch( (e) => {
+                    onFinish( null );
+                    return null
+                } );
         }
     }
 }
@@ -115,4 +120,61 @@ export function getRepeaterOptionFromRepeaters( repeaters, repeaterSlug ) {
         });
     }
     return repeaterOption;
+}
+
+
+/**
+ * Try Parsing
+ */
+function TryParseJSON( jsonString, forceJson = true ) {
+	try {
+		var o = JSON.parse( jsonString );
+
+		// Handle non-exception-throwing cases:
+		// Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+		// but... JSON.parse(null) returns null, and typeof null === "object", 
+		// so we must check for that, too. Thankfully, null is falsey, so this suffices:
+		if (o && typeof o === "object") {
+			return o;
+		}
+	}
+	catch (e) { }
+	if ( jsonString && typeof jsonString === "object" ) {
+		return jsonString;
+	}
+	if ( forceJson ) {
+		return {};
+	}
+	return false;
+}
+
+function getQuery( blockAttributes, contextPost = null, repeaterRow = '', dynamicSource = '' ) {
+	const { source, field, custom, para, before, after, fallback, type, relate, relcustom, showAll, useRepeaterContext } = blockAttributes;
+	let theSource = source ? source : contextPost;
+	// This can be removed once fully updated to use kadenceblockspro/data store.
+	if ( wp.data.select( 'core/editor' ) && ! theSource ) {
+		if ( kbpData.isKadenceE && kadenceElementParams.previewPostID ) {
+			const postId = TryParseJSON( kadenceElementParams.previewPostID );
+			theSource = postId && postId.id ? postId.id : '';
+		} else {
+			theSource = wp.data.select( 'core/editor' ).getCurrentPostId();
+		}
+	}
+	dynamicSource = dynamicSource ? dynamicSource : theSource;
+
+	return {
+		source: useRepeaterContext ? dynamicSource : theSource,
+		field: ( field ? field : '' ),
+		custom: ( custom ? custom : '' ),
+		para: ( para ? para : '' ),
+		before: ( before ? before : '' ),
+		after: ( after ? after : '' ),
+		fallback: ( fallback ? fallback : '' ),
+		type: ( type ? type : '' ),
+		relate: ( relate ? relate : '' ),
+		relcustom: ( relcustom ? relcustom : '' ),
+		ignore_source: ( showAll ? showAll : false ),
+		useRepeaterContext: ( useRepeaterContext ? useRepeaterContext : false ),
+		repeaterRow: repeaterRow,
+	};
 }
