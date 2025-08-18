662b34c2c24927b9ec4ccb01b408f6a381719301
Josh Oakes <josh@joshoak.es>
2025-08-18 16:02:25 -0500

    Match free changes

--- src/get-unique-id/index.js
+++ src/get-unique-id/index.js
@@ -4,6 +4,7 @@ import getPostOrWidgetId from '../get-post-or-widget-id';
 import { useEffect, useMemo } from '@wordpress/element';
 import { get, has } from 'lodash';
 import { default as hashString } from '../hash-string';
+import getBlockDefaults from '../set-block-defaults';
 
 /**
  * Creates or keeps a uniqueId for a block depending on it's status.
@@ -35,17 +36,33 @@ export default function uniqueIdHelper(props) {
 		[select, clientId]
 	);
 
-	const updateUniqueID = (newUniqueID) => {
+	const updateUniqueID = (newUniqueID, isNewBlock = false) => {
 		if (!isUniqueID(newUniqueID)) {
 			// Solves for rare case where the client id matches enough that our unique id is not unique.
 			newUniqueID = uniqueId(newUniqueID);
 		}
-		// Set the unique ID in the attributes and the store. On the attribute variable is important for race conditions.
-		attributes.uniqueID = newUniqueID;
-		setAttributes({ uniqueID: newUniqueID });
+		
+		let updatedAttributes = { uniqueID: newUniqueID };
+		
+		// Apply block defaults for new blocks
+		if (isNewBlock) {
+			const defaultedAttributes = getBlockDefaults(props.name, attributes);
+			updatedAttributes = {
+				...defaultedAttributes,
+				uniqueID: newUniqueID
+			};
+		}
+		
+		// Update attributes on the object for race conditions
+		Object.assign(attributes, updatedAttributes);
+		
+		// Set all attributes in one call
+		setAttributes(updatedAttributes);
+		
+		// Add to store
 		addUniqueID(newUniqueID, clientId);
 	};
-	useEffect(() => {
+	const getBlockPostId = (parentData) => {
 		const { postId, reusableParent, rootBlock, editedPostId } = parentData;
 		let blockPostId = getPostOrWidgetId(props, postId, reusableParent, 0);
 		if (blockPostId === 0) {
@@ -56,7 +73,22 @@ export default function uniqueIdHelper(props) {
 				blockPostId = hashString(editedPostId) % 1000000;
 			}
 		}
+		
+		return blockPostId;
+	};
 
+	// Set uniqueID immediately if not defined
+	if (!attributes?.uniqueID ) {
+		const blockPostId = getBlockPostId(parentData);
+		const blockPostIdPrefix = blockPostId ? blockPostId + '_' : '';
+		const newUniqueID = blockPostIdPrefix + clientId.substr(2, 9);
+		
+		// This is a new block
+		updateUniqueID(newUniqueID, true);
+	}
+
+	useEffect(() => {
+		const blockPostId = getBlockPostId(parentData);
 		const hasBlockPostIdPrefix = attributes?.uniqueID && attributes?.uniqueID.split('_').length === 2;
 		const blockPostIdPrefix = blockPostId ? blockPostId + '_' : '';
 		const newUniqueID = blockPostIdPrefix + clientId.substr(2, 9);
@@ -66,10 +98,10 @@ export default function uniqueIdHelper(props) {
 			(hasBlockPostIdPrefix && attributes?.uniqueID.split('_')[0] !== blockPostId.toString())
 		) {
 			// New block
-			return updateUniqueID(newUniqueID);
+			return updateUniqueID(newUniqueID, true);
 		} else if (!isUniqueID(attributes?.uniqueID) && !isUniqueBlock(attributes?.uniqueID, clientId)) {
 			// This checks if we are just switching views, client ID the same means we don't need to update but otherwise we assume it's a duplicate block.
-			return updateUniqueID(newUniqueID);
+			return updateUniqueID(newUniqueID, false);
 		}
 		// This just logs the block in the store and it doesn't need to be updated.
 		addUniqueID(attributes?.uniqueID, clientId);
